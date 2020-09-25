6b9eabe6460f823c3756528af22b2a0c7e11398c
robert-neumann <62569364+robert-neumann@users.noreply.github.com>
2020-09-25 02:42:39 +0100

    Add files via upload

---
+++ precache-manifest.a1280f016e9ab49c17fa7c05710b1f8a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25fb5897480afce2af8e4ed0566979b9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d9b1dda3afb1f947d850",
+    "url": "./static/css/main.a7ba7a49.chunk.css"
+  },
+  {
+    "revision": "e55fc5d81c1beafed501",
+    "url": "./static/js/2.2d178d24.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2d178d24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9b1dda3afb1f947d850",
+    "url": "./static/js/main.949d7313.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  },
+  {
+    "revision": "0cee1742130655c08863831d928b4517",
+    "url": "./static/media/city.0cee1742.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
